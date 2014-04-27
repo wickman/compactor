234efa0267d1921463aed59e3369e041f0497c9a
Brian Wickman <wickman@twitter.com>
2014-04-27 12:10:13 -0700

    Add LICENSE and setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+import sys
+from setuptools import setup
+
+
+__version__ = '0.1.0'
+
+
+install_requires = [
+  'trollius',
+  'tornado>=3',
+  'twitter.common.lang',
+]
+
+
+setup(
+  name='compactor',
+  version=__version__,
+  description='a pure python implementation of libprocess actors',
+  url='http://github.com/wickman/compactor',
+  author='Brian Wickman',
+  author_email='wickman@gmail.com',
+  license='Apache License 2.0',
+  package_dir={'': 'src'},
+  packages=['compactor'],
+  install_requires=install_requires,
+  zip_safe=True,
+)
