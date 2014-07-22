a481a0d093d85069803b5245abc6ec6ed6b5afae
Tom Arnfeld <tarnfeld@me.com>
2014-07-22 11:30:01 +0100

    Fixes to __init__.py and PEP8

--- src/compactor/__init__.py
+++ src/compactor/__init__.py
@@ -1,5 +1,4 @@
 import functools
-import threading
 
 from .context import Context
 from .process import Process
@@ -9,40 +8,39 @@ _ROOT_CONTEXT = None
 
 
 def initialize(delegate="", **kw):
-  global _ROOT_CONTEXT
-  _ROOT_CONTEXT = Context.singleton(delegate=delegate, **kw)
-  print(_ROOT_CONTEXT)
-  if not _ROOT_CONTEXT.is_alive():
-    _ROOT_CONTEXT.start()
+    global _ROOT_CONTEXT
+    _ROOT_CONTEXT = Context.singleton(delegate=delegate, **kw)
+    if not _ROOT_CONTEXT.is_alive():
+        _ROOT_CONTEXT.start()
 
 
 def join():
-  _ROOT_CONTEXT.join()
+    _ROOT_CONTEXT.join()
 
 
 def after_init(fn):
-  @functools.wraps(fn)
-  def wrapper_fn(*args, **kw):
-    initialize()
-    return fn(*args, **kw)
-  return wrapper_fn
+    @functools.wraps(fn)
+    def wrapper_fn(*args, **kw):
+        initialize()
+        return fn(*args, **kw)
+    return wrapper_fn
 
 
 @after_init
 def spawn(process):
-  """Spawn a process and return its pid."""
-  return _ROOT_CONTEXT.spawn(process)
+    """Spawn a process and return its pid."""
+    return _ROOT_CONTEXT.spawn(process)
 
 
 @after_init
 def link(process, to):
-  return _ROOT_CONTEXT.link(process, to)
+    return _ROOT_CONTEXT.link(process, to)
 
 
 @after_init
 def send(to, name, data=None):
-  """Send data to a remote process at `to` with the name `name`."""
-  return _ROOT_CONTEXT.send(to, name, data=data)
+    """Send data to a remote process at `to` with the name `name`."""
+    return _ROOT_CONTEXT.send(to, name, data=data)
 
 
 del after_init
@@ -50,16 +48,15 @@ del after_init
 
 route = Process.route
 install = Process.install
-from tornado.gen import Task as task
 
 
 __all__ = (
-  'initialize',
-  'install',
-  'join',
-  'link',
-  'route',
-  'task',
-  'send',
-  'spawn',
+    'initialize',
+    'install',
+    'join',
+    'link',
+    'route',
+    'task',
+    'send',
+    'spawn',
 )
