02e46a7be888d303a82cf4d7b9aebe74cb01f393
Tom Arnfeld <tarnfeld@me.com>
2014-07-22 17:37:11 +0100

    Tidy up the request module

--- src/compactor/request.py
+++ src/compactor/request.py
@@ -1,30 +1,33 @@
+
 CRLF = b'\r\n'
 
 
-def encode_request(
-        from_process,
-        to_process,
-        method,
-        body=None,
-        content_type=None,
-        legacy=False):  # set legacy=True if we should use User-Agent based identification
+def encode_request(from_pid, to_pid, method, body=None, content_type=None, legacy=False):
+    """
+    Encode a request into a raw HTTP request. This function returns a string
+    of bytes that represent a valid HTTP/1.0 request, including any libprocess
+    headers required for communication.
 
-    body = body or b''
+    Use the `legacy` option (set to True) to use the legacy User-Agent based
+    libprocess identification.
+    """
+
+    if body is None:
+        body = b''
 
     headers = [
-        'POST /{process}/{method} HTTP/1.0'.format(process=to_process.id, method=method),
+        'POST /{process}/{method} HTTP/1.0'.format(process=to_pid.id, method=method),
         'Connection: Keep-Alive',
+        'Content-Length: %d' % len(body)
     ]
 
     if legacy:
-        headers.append('User-Agent: libprocess/{pid}'.format(pid=from_process))
+        headers.append('User-Agent: libprocess/{pid}'.format(pid=from_pid))
     else:
-        headers.append('Libprocess-From: {pid}'.format(pid=from_process))
+        headers.append('Libprocess-From: {pid}'.format(pid=from_pid))
 
     if content_type is not None:
         headers.append('Content-Type: {content_type}'.format(content_type=content_type))
-
-    headers.append('Content-Length: {length}'.format(length=len(body)))
 
     headers = [header.encode('utf8') for header in headers]
 
