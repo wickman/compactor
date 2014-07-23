c1fc8b543316efa616a75df1d2f43b86aebdec87
Tom Arnfeld <tarnfeld@me.com>
2014-07-23 14:24:20 +0100

    Cleanup setup.py and support for pip install
    The BUILD files conflict with the default build/ directory so we'll set a default build base that doesn't conflict.

--- setup.py
+++ setup.py
@@ -1,30 +1,25 @@
-import sys
 from setuptools import setup
-
 
 __version__ = '0.1.0'
 
 
-install_requires = [
-  'trollius',
-  'tornado>=3',
-  'twitter.common.lang',
-]
-
-
 setup(
-  name='compactor',
-  version=__version__,
-  description='a pure python implementation of libprocess actors',
-  url='http://github.com/wickman/compactor',
-  author='Brian Wickman',
-  author_email='wickman@gmail.com',
-  license='Apache License 2.0',
-  package_dir={'': 'src'},
-  packages=['compactor'],
-  install_requires=install_requires,
-  extras_require={
-    'pb': ['protobuf'],
-  },
-  zip_safe=True,
+    name='compactor',
+    version=__version__,
+    description='Pure python implementation of libprocess actors',
+    url='http://github.com/wickman/compactor',
+    author='Brian Wickman',
+    author_email='wickman@gmail.com',
+    license='Apache License 2.0',
+    package_dir={'': 'src'},
+    packages=['compactor'],
+    install_requires=[
+        'trollius',
+        'tornado>=3',
+        'twitter.common.lang',
+    ],
+    extras_require={
+        'pb': ['protobuf'],
+    },
+    zip_safe=True,
 )
