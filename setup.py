64b449a8a40d8e1a98b133383d828c5a92114e0b
Tom Arnfeld <tarnfeld@me.com>
2014-08-21 09:53:27 -0500

    Indentation for setup.py

--- setup.py
+++ setup.py
@@ -4,22 +4,22 @@ __version__ = '0.1.0'
 
 
 setup(
-    name='compactor',
-    version=__version__,
-    description='Pure python implementation of libprocess actors',
-    url='http://github.com/wickman/compactor',
-    author='Brian Wickman',
-    author_email='wickman@gmail.com',
-    license='Apache License 2.0',
-    package_dir={'': 'src'},
-    packages=['compactor'],
-    install_requires=[
-        'trollius',
-        'tornado>=3',
-        'twitter.common.lang',
-    ],
-    extras_require={
-        'pb': ['protobuf'],
-    },
-    zip_safe=True,
+  name='compactor',
+  version=__version__,
+  description='Pure python implementation of libprocess actors',
+  url='http://github.com/wickman/compactor',
+  author='Brian Wickman',
+  author_email='wickman@gmail.com',
+  license='Apache License 2.0',
+  package_dir={'': 'src'},
+  packages=['compactor'],
+  install_requires=[
+    'trollius',
+    'tornado>=3',
+    'twitter.common.lang',
+  ],
+  extras_require={
+    'pb': ['protobuf'],
+  },
+  zip_safe=True,
 )
